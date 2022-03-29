42da992de414cb4f96129732b0bcc6539080a7e8
yinglou727 <102577554+yinglou727@users.noreply.github.com>
2022-03-29 15:44:57 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1648536931,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/hammer-sheet0.png",
+		"images/momo-sheet0.png",
+		"images/momo-sheet1.png",
+		"images/hole-sheet0.png",
+		"media/bbzaq-tm27j.m4a",
+		"media/bomb (online-audio-converter.com).m4a",
+		"media/bomb (online-audio-converter.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
